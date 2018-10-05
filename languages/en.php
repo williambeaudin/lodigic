1a9aaceb796059a3964cfe374a0c5d5598a5089d
丁william <william_686@hotmail.com>
2018-10-05 11:06:37 +0630

    Add multilanguage option
    Converted index.html into index.php added config.php and languages files updated some html, css and js

---
+++ languages/en.php
@@ -0,0 +1,84 @@
+<?php
+    $lang = array(
+      "title" => "Lodgic Solutions | Digital World",
+      //nav
+      // "home" => "Home",
+      "about" => "About",
+      "projects" => "Projects",
+      "services" => "Services",
+      "team" => "Team",
+      "contact" => "Contact us",
+
+      "quote" => "LOGIC SOLUTIONS,IN A DIGITAL WORLD",
+      //about
+      // "about_description" => "Fan Designing is a graphic design and web development team that provides customers with bespoke, professional marketing/design solutions.<br><br>The global marketplace is a dynamic and highly competitive environment and to stand out, your brand and marketing strategy should be dynamic and innovative.<br><br>Using the tools of graphic design and web development, Fan Designing will facilitate your business’s growth and establish a product that will stand the test of time.",
+      //  //icons
+      // "photo_editing" => "photo editing",
+      // "illustration" => "illustration",
+      // "art_editing" => "art editing",
+      // //projects
+      // "graphics" => "graphics",
+      // "illustrations" => "illustrations",
+      // "exhibitions" => "exhibitions",
+      // "web" => "web",
+      // //services
+      // "services_title" => "what we can do",
+      // "graphic_design" => "graphic design",
+      // "brochure" => "Brochure",
+      // "text_formatting" => "Text formatting",
+      // "brand_identity" => "Brand identity",
+      // "ppt_presentation" => "PPT presentation",
+      // "business_card" => "Business card",
+      // "key_visual" => "Key visual",
+      // "flyer_poster" => "Flyer/poster",
+      // "banner" => "Banner",
+      // "online_marketing_material" => "Online Marketing Material",
+      // "custom" => "Custom",
+      // "medical" => "Medical",
+      // "scientific" => "Scientific",
+      // "fashion" => "Fashion",
+      // "exhibition_design" => "EXHIBITION DESIGN",
+      // "event_identity" => "Event identity",
+      // "promotional_material" => "Promotional material",
+      // "vynil" => "Vinyl/pop-up/roll-up banners",
+      // "backdrop" => "Backdrop",
+      // "badge" => "Badge",
+      // "web_design" => "WEB DESIGN",
+      // "custom_html" => "Custom HTML/CSS",
+      // "responsive" => "Responsive Design",
+      // "bootstrap" => "Bootstrap/jQuery",
+      // "email" => "Email Redirection",
+      // "domain" => "Domain Name",
+      // "social_media" => "Social Media",
+      // "wordpress" => "Wordpress",
+      // "hosting" => "Hosting",
+      // "seo" => "SEO",
+      //
+      // //team
+      // "fan_title" => "Graphic Design",
+      // "fan_bio" => "Fan, a graduate of Fu-Hsin Trade & Arts School, Taipei, and later the Taiwan Art University has always had a passion for design and visual arts. After graduating, she pursued a career in photography, where she worked as a commercial photography assistant.<br><br>Later, she made the shift to graphic design where she worked on exhibitions for a variety of well known brands, such as Lipton, Children Are Us Foundation and Falglory Life Insurance.<br><br>Now, after establishing her freelance brand - Fan Designing -  Fan is applying her skills and experience from years in the industry to bring her customers the timeless graphic design solutions they need.",
+      //
+      //   "will_title" => "Web design",
+      //   "will_bio" => "William, a Mechanical Engineering graduate of Cegep de Saint Jerome, Quebec, has spent a considerable amount of his life traveling and volunteering in over 35 countries around the globe. His engineering mind and fascination with experiencing new ideas led him to find his passion in the dynamic world of the digital, in particular online marketing and web development.<br><br>In order to further fulfil this dream, he enrolled in courses in HTML, CSS, JavaScript and PHP coding systems as well as SEO, hosting, Word Press and Instagram. At Fan Designing, William is applying these skills to bring their clients’ ideas to life.
+      //   ",
+      //
+      //
+      // //Form
+      // 'name'     => 'Name',
+      // 'email'    => 'Email',
+      // 'subject'  => 'Subject',
+      // 'message'  => 'Message',
+      // 'btn_send' => 'Send',
+      //
+      // 'error'   => 'There was an error sending, please try again later.',
+      // 'success' => 'Your message has been sent successfully.',
+      //
+      // 'coming_soon' => 'Coming soon',
+      //
+      // 'see_website' => 'VISIT WEBSITE',
+
+      //languages
+      "lang_en" => "english",
+      "lang_fr" => "french"
+    );
+  ?>
